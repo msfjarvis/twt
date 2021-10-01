61cf4d2f8e8795ce7035c8f1b09fd02abb67afc5
Nerixyz <nero.9@hotmail.de>
2021-10-01 18:28:39 +0200

    feat: add optional MAX_AMOUNT config
    Closes #2

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ async fn main() -> Result<()> {
     let access_token = std::env::var("ACCESS_TOKEN")?;
     let access_token_secret = std::env::var("ACCESS_TOKEN_SECRET")?;
     let username = std::env::var("TARGET_USERNAME")?;
+    let page_size = std::env::var("MAX_AMOUNT")
+        .ok()
+        .and_then(|amount| amount.parse().ok())
+        .unwrap_or(1024);
 
     let consumer = KeyPair::new(consumer_key, consumer_secret);
     let access = KeyPair::new(access_token, access_token_secret);
@@ -20,7 +24,7 @@ async fn main() -> Result<()> {
 
     let user_id: UserID = username.into();
 
-    let timeline = tweet::user_timeline(user_id, false, false, &token).with_page_size(1024);
+    let timeline = tweet::user_timeline(user_id, false, false, &token).with_page_size(page_size);
     let (_, feed) = timeline.start().await?;
     print_urls(feed.iter());
 
