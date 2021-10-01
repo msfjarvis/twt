5e4b5432bb4ca97423901304ddc618f1f206142f
Nerixyz <nero.9@hotmail.de>
2021-10-01 19:53:55 +0200

    feat: add cli options

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,50 @@ use egg_mode::tweet;
 use egg_mode::user::UserID;
 use egg_mode::KeyPair;
 use egg_mode::Token::Access;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(
+    rename_all = "kebab-case",
+    about = "Fetches the last tweets of a given account, then prints original quality URLs for all image tweets."
+)]
+struct CliOptions {
+    /// The Twitter username of the account to fetch images from.
+    username: String,
+
+    /// The maximum amount of tweets to check for images.
+    #[structopt(long, default_value = "1024")]
+    max_amount: i32,
+
+    /// The consumer API key for the project.
+    #[structopt(long, env)]
+    consumer_key: String,
+
+    /// The consumer secret for the project.
+    #[structopt(long, env)]
+    consumer_secret: String,
+
+    /// The access token for your user, for the project.
+    #[structopt(long, env)]
+    access_token: String,
+
+    /// The access token secret for your user.
+    #[structopt(long, env)]
+    access_token_secret: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let consumer_key = std::env::var("CONSUMER_KEY")?;
-    let consumer_secret = std::env::var("CONSUMER_KEY_SECRET")?;
-    let access_token = std::env::var("ACCESS_TOKEN")?;
-    let access_token_secret = std::env::var("ACCESS_TOKEN_SECRET")?;
-    let username = std::env::var("TARGET_USERNAME")?;
-    let page_size = std::env::var("MAX_AMOUNT")
-        .ok()
-        .and_then(|amount| amount.parse().ok())
-        .unwrap_or(1024);
+    let options: CliOptions = CliOptions::from_args();
 
-    let consumer = KeyPair::new(consumer_key, consumer_secret);
-    let access = KeyPair::new(access_token, access_token_secret);
+    let consumer = KeyPair::new(options.consumer_key, options.consumer_secret);
+    let access = KeyPair::new(options.access_token, options.access_token_secret);
     let token = Access { consumer, access };
 
-    let user_id: UserID = username.into();
+    let user_id: UserID = options.username.into();
 
-    let timeline = tweet::user_timeline(user_id, false, false, &token).with_page_size(page_size);
+    let timeline =
+        tweet::user_timeline(user_id, false, false, &token).with_page_size(options.max_amount);
     let (_, feed) = timeline.start().await?;
     print_urls(feed.iter());
 
