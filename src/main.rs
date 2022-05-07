3453bb5b1bd70da3303d3e0b07d95200f11b3a77
Harsh Shandilya <me@msfjarvis.dev>
2022-05-07 15:33:36 +0530

    feat: replace structopt with clap v3 and update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,45 @@
 use std::slice::Iter;
 
+use clap::{AppSettings, Parser};
 use color_eyre::Result;
 use egg_mode::tweet;
 use egg_mode::user::UserID;
 use egg_mode::KeyPair;
 use egg_mode::Token::Access;
-use structopt::StructOpt;
 
-#[derive(StructOpt)]
-#[structopt(
-    rename_all = "kebab-case",
-    about = "Fetches the last tweets of a given account, then prints original quality URLs for all image tweets."
-)]
+#[derive(Parser)]
+#[clap(author, version, about)]
+#[clap(global_setting(AppSettings::DeriveDisplayOrder))]
+/// Fetches the last tweets of a given account, then prints original quality URLs for all image tweets.
 struct CliOptions {
     /// The Twitter username of the account to fetch images from.
-    #[structopt(env = "TARGET_USERNAME")]
+    #[clap(env = "TARGET_USERNAME")]
     username: String,
 
     /// The maximum amount of tweets to check for images.
-    #[structopt(long, default_value = "1024")]
+    #[clap(long, default_value = "1024")]
     max_amount: i32,
 
     /// The consumer API key for the project.
-    #[structopt(long, env, default_value = std::option_env!("CONSUMER_KEY").unwrap_or(""))]
+    #[clap(long, env, default_value = std::option_env!("CONSUMER_KEY").unwrap_or(""))]
     consumer_key: String,
 
     /// The consumer key secret for the project.
-    #[structopt(long, env, default_value = std::option_env!("CONSUMER_KEY_SECRET").unwrap_or(""))]
+    #[clap(long, env, default_value = std::option_env!("CONSUMER_KEY_SECRET").unwrap_or(""))]
     consumer_key_secret: String,
 
     /// The access token for your user, for the project.
-    #[structopt(long, env, default_value = std::option_env!("ACCESS_TOKEN").unwrap_or(""))]
+    #[clap(long, env, default_value = std::option_env!("ACCESS_TOKEN").unwrap_or(""))]
     access_token: String,
 
     /// The access token secret for your user.
-    #[structopt(long, env, default_value = std::option_env!("ACCESS_TOKEN_SECRET").unwrap_or(""))]
+    #[clap(long, env, default_value = std::option_env!("ACCESS_TOKEN_SECRET").unwrap_or(""))]
     access_token_secret: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let options: CliOptions = CliOptions::from_args();
+    let options = CliOptions::parse();
 
     let consumer = KeyPair::new(options.consumer_key, options.consumer_key_secret);
     let access = KeyPair::new(options.access_token, options.access_token_secret);
