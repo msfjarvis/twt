5fc21977b6c32b856db32c2280a40186eda3b402
Harsh Shandilya <me@msfjarvis.dev>
2022-05-29 10:08:12 +0530

    feat: add Nix shell configuration

---
+++ shell.nix
@@ -0,0 +1,14 @@
+with import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; };
+mkShell {
+  buildInputs = [
+    (rust-bin.stable.latest.default.override {
+      extensions =
+        [ "rust-src" "rustc-dev" "rustfmt-preview" "llvm-tools-preview" ];
+      targets =
+        pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
+        ++ pkgs.lib.optionals pkgs.stdenv.isLinux
+        [ "x86_64-unknown-linux-gnu" ];
+    })
+  ] ++ pkgs.lib.optionals stdenv.isDarwin
+    [ pkgs.darwin.apple_sdk.frameworks.Security ];
+}
