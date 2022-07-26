cc646147e53b44e650cc89f4b28ee9b720e7e61e
Harsh Shandilya <me@msfjarvis.dev>
2022-07-27 00:58:44 +0530

    fix(nix): use Rust from nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,14 +1,9 @@
-with import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; };
-mkShell {
-  buildInputs = [
-    (rust-bin.stable.latest.default.override {
-      extensions =
-        [ "rust-src" "rustc-dev" "rustfmt-preview" "llvm-tools-preview" ];
-      targets =
-        pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
-        ++ pkgs.lib.optionals pkgs.stdenv.isLinux
-        [ "x86_64-unknown-linux-gnu" ];
-    })
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    cargo
+    rustc
+    rustfmt
   ] ++ pkgs.lib.optionals stdenv.isDarwin
     [ pkgs.darwin.apple_sdk.frameworks.Security ];
 }
