2059f656ef3957924ce07ccfd3765e9ccbaadea8
Harsh Shandilya <me@msfjarvis.dev>
2022-07-29 15:00:21 +0530

    fix: resolve clippy warnings

--- src/main.rs
+++ src/main.rs
@@ -161,17 +161,17 @@ async fn main() -> Result<()> {
 // It'd be significantly easier to just use [Vec::sort_by] but lol, lmao.
 // (It's actually because we don't have a mutable reference to the Vec).
 fn find_largest_video(videos: &Vec<VideoVariant>) -> &VideoVariant {
-    let mut largest = None;
+    let mut largest: Option<&VideoVariant> = None;
     for video in videos {
         if let Some(bitrate) = video.bitrate {
-            if largest.is_none() {
-                largest = Some(video);
-            } else if bitrate > largest.unwrap().bitrate.unwrap() {
+            if largest.is_none()
+                || (largest.is_some() && bitrate > largest.unwrap().bitrate.unwrap())
+            {
                 largest = Some(video);
             }
         }
     }
-    return largest.unwrap();
+    largest.unwrap()
 }
 
 fn print_video_urls(iterator: Iter<'_, tweet::Tweet>) {
@@ -180,7 +180,7 @@ fn print_video_urls(iterator: Iter<'_, tweet::Tweet>) {
         .flat_map(|entities| &entities.media)
         .flat_map(|x| &x.video_info)
         .map(|x| &x.variants)
-        .flat_map(|variants| {
+        .filter_map(|variants| {
             if variants.is_empty() {
                 return None;
             }
