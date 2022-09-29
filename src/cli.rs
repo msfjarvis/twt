56e16f0245ca374f7afd977021ed97969cc39bf5
Harsh Shandilya <me@msfjarvis.dev>
2022-09-29 11:42:18 +0530

    Migrate to clap v4

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,4 @@
-use clap::{AppSettings, Args, Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 use egg_mode::{
     tweet::{self, Timeline},
     user::UserID,
@@ -17,47 +17,46 @@ pub trait TimelineCreator {
 }
 
 #[derive(Parser)]
-#[clap(author, version, about)]
-#[clap(global_setting(AppSettings::DeriveDisplayOrder))]
+#[command(author, version, about)]
 /// Fetches the last tweets of a given account, then prints original quality URLs for all image tweets.
 pub struct Opts {
-    #[clap(subcommand)]
+    #[command(subcommand)]
     pub command: Commands,
 }
 
 #[derive(Debug, Subcommand)]
 pub enum Commands {
-    #[clap(arg_required_else_help = true)]
+    #[command(arg_required_else_help = true)]
     Images(Images),
-    #[clap(arg_required_else_help = true)]
+    #[command(arg_required_else_help = true)]
     Links(Links),
-    #[clap(arg_required_else_help = true)]
+    #[command(arg_required_else_help = true)]
     Videos(Videos),
 }
 
 #[derive(Debug, Args)]
 pub struct CommonCliOpts {
     /// The Twitter username of the account to fetch images from.
-    #[clap(long)]
+    #[arg(long)]
     pub username: String,
 
     /// The maximum amount of tweets to check for images.
-    #[clap(long, default_value = "1024")]
+    #[arg(long, default_value = "1024")]
     pub max_amount: i32,
 
     /// Include retweets.
-    #[clap(long, default_value_t = false, value_parser = clap::value_parser!(bool))]
+    #[arg(long, default_value_t = false, value_parser = clap::value_parser!(bool))]
     pub with_rts: bool,
 
     /// Include replies.
-    #[clap(long, default_value_t = false, value_parser = clap::value_parser!(bool))]
+    #[arg(long, default_value_t = false, value_parser = clap::value_parser!(bool))]
     pub with_replies: bool,
 }
 
 #[derive(Debug, Args)]
 /// Fetch original quality images from the tweets of a given Twitter user
 pub struct Images {
-    #[clap(flatten)]
+    #[command(flatten)]
     opts: CommonCliOpts,
 }
 
@@ -70,11 +69,11 @@ impl TimelineCreator for Images {
 #[derive(Debug, Args)]
 /// Fetch links from the user's tweets, filtered based on the given host.
 pub struct Links {
-    #[clap(flatten)]
+    #[command(flatten)]
     opts: CommonCliOpts,
 
     /// The host name to filter links on.
-    #[clap(long, default_value = "imgur.com")]
+    #[arg(long, default_value = "imgur.com")]
     pub host: String,
 }
 
@@ -87,12 +86,22 @@ impl TimelineCreator for Links {
 #[derive(Debug, Args)]
 /// Fetch high quality versions of videos embedded in the user's tweets.
 pub struct Videos {
-    #[clap(flatten)]
+    #[command(flatten)]
     opts: CommonCliOpts,
 }
 
 impl TimelineCreator for Videos {
     fn timeline(&self, token: Token) -> Timeline {
         self.create_timeline(&self.opts, token)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Opts;
+
+    #[test]
+    fn cli_assert() {
+        <Opts as clap::CommandFactory>::command().debug_assert()
     }
 }
