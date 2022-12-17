3cb28113dd57391716e159a2cf446ac6316bc5fa
Harsh Shandilya <me@msfjarvis.dev>
2022-12-17 06:09:33 +0000

    feat(nix): migrate build and CI to Nix Flakes
    Fixes SDP-32

--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    cargo
-    rustc
-    rustfmt
-  ] ++ pkgs.lib.optionals stdenv.isDarwin
-    [ pkgs.darwin.apple_sdk.frameworks.Security ];
-}
